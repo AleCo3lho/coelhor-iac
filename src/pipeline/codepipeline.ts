631a8e25657d8f3527dae7f8e8a4107459f32fb4
Aleco3lho <alexandre_ramos007@hotmail.com>
2023-11-14 20:55:11 -0400

    first commit

--- src/pipeline/codepipeline.ts
+++ src/pipeline/codepipeline.ts
@@ -1,13 +1,19 @@
 import { Stack, StackProps, Stage, StageProps, Tags } from "aws-cdk-lib";
 import { CodePipeline, CodePipelineSource, ShellStep } from "aws-cdk-lib/pipelines";
 import { Construct } from "constructs";
-import { CHANGE_ME_Stack } from "../stack";
+import { CoelhorIac } from "../stack";
+import { prodConfig } from "../config";
 
 export class PipelineStage extends Stage {
   constructor(scope: Construct, id: string, props?: StageProps) {
     super(scope, id, props);
 
-    new CHANGE_ME_Stack(this, "StageStack", {});
+    new CoelhorIac(this, "InfraStack", {
+      env: {
+        account: `${prodConfig.env.account}`,
+        region: `${prodConfig.env.region}`,
+      },
+    });
   }
 }
 
@@ -18,34 +24,28 @@ export class CodePipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // Pipeline source: branch and Github org/repository
     const branch = "main";
-    const githubRepository = "org/repo";
-    /**
-     * A Codestar connection to Github must be manually created using the AWS console
-     * see https://docs.aws.amazon.com/codepipeline/latest/userguide/update-github-action-connections.html
-     */
+    const githubRepository = "AleCo3lho/coelhor-iac";
+
     const githubConnection = CodePipelineSource.connection(githubRepository, branch, {
       connectionArn:
-        "arn:aws:codestar-connections:us-east-1:222222222222:connection/7d2469ff-514a-4e4f-9003-5ca4a43cdc41",
+        "arn:aws:codestar-connections:us-east-1:239828624774:connection/c6e747f6-7996-4583-bdb6-fe659a6ab9e3",
     });
 
     const pipeline = new CodePipeline(this, "PipelineStack", {
-      pipelineName: "example-codepipeline",
+      pipelineName: "SynthPipeline",
       synth: new ShellStep("Synth", {
         input: githubConnection,
         commands: ["yarn", "yarn run cdk synth"],
         primaryOutputDirectory: "cdk.out",
       }),
-      selfMutation: true, // Whether the pipeline will update itself.
-      crossAccountKeys: false, // Create KMS keys for the artifact buckets, allowing cross-account deployments.
+      selfMutation: true,
+      crossAccountKeys: false,
     });
 
-    // A pipeline can have multiple stages (for ex. dev, staging, prod)
-    pipeline.addStage(new PipelineStage(this, "PipelineStage", {}));
+    pipeline.addStage(new PipelineStage(this, "prod", {}));
 
-    // Please configure the proper tags for this project
-    Tags.of(this).add("Project", "Name of project");
-    Tags.of(this).add("Team", "Name of team");
+    Tags.of(this).add("Project", "coelhor-iac");
+    Tags.of(this).add("Author", "Alexandre Coelho Ramos");
   }
 }
--- test/main.test.ts
+++ test/main.test.ts
@@ -1,13 +1,18 @@
 import * as cdk from "aws-cdk-lib";
 import { Template } from "aws-cdk-lib/assertions";
-
-import { CHANGE_ME_Stack } from "../src/stack";
+import { prodConfig } from "../src/config";
+import { CoelhorIac } from "../src/stack";
 import { CodePipelineStack } from "../src/pipeline/codepipeline";
 
 // Test for single stack
-test("snapshot for CHANGE_ME_Stack matches previous state", () => {
+test("snapshot for CoelhorIac matches previous state", () => {
   const app = new cdk.App();
-  const stack = new CHANGE_ME_Stack(app, "MyTestStack");
+  const stack = new CoelhorIac(app, "MyTestStack", {
+    env: {
+      account: `${prodConfig.env.account}`,
+      region: `${prodConfig.env.region}`,
+    },
+  });
 
   const template = Template.fromStack(stack);
   expect(template.toJSON()).toMatchSnapshot();
@@ -16,7 +21,12 @@ test("snapshot for CHANGE_ME_Stack matches previous state", () => {
 // Test for pipeline stack
 test("snapshot for CodePipelineStack matches previous state", () => {
   const app = new cdk.App();
-  const stack = new CodePipelineStack(app, "MyCodePipelineStack");
+  const stack = new CodePipelineStack(app, "MyCodePipelineStack", {
+    env: {
+      account: `${prodConfig.env.account}`,
+      region: `${prodConfig.env.region}`,
+    },
+  });
 
   const template = Template.fromStack(stack);
   expect(template.toJSON()).toMatchSnapshot();
